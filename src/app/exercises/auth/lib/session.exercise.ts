e91ab3f8213d61612eadb34dbffac7b864c81a49
Ons HAMAD <onshamadkl@gmail.com>
2024-10-11 18:38:36 +0100

    correction

--- src/app/exercises/auth/lib/session.exercise.ts
+++ src/app/exercises/auth/lib/session.exercise.ts
@@ -1,9 +1,9 @@
 /* eslint-disable no-restricted-imports */
 
-// 🐶 importe le type (SessionType)
+// 🐶 Importe le type `SessionType`
 // 🤖 import {SessionType} from './type'
 
-// 🐶 importe les fonctions de session database
+// 🐶 Importe les fonctions de session database
 // 🤖
 // import {
 //   createSession as createSessionDb,
@@ -18,30 +18,30 @@ import {
   verifySession as verifySessionStateless,
 } from './session-stateless'
 
-// 🐶 récupère le type de session défini en variable d'environnement
+// 🐶 Récupère le type de session définie en variable d'environnement
 // 🤖 const SESSION_TYPE: SessionType = process.env.SESSION_TYPE as SessionType
 
-// 🐶 Implemente le Strategy Pattern
+// 🐶 Implémente le `Strategy Pattern`
 export async function createSession(userId: string) {
-  // 🐶 crée une session en fonction du type de session défini
-  // tu peux utiliser un switch case pour appeler la bonne fonction
+  // 🐶 Crée une session en fonction du type de session définie
+  // Tu peux utiliser un `switch case` pour appeler la bonne fonction
   // 🤖 switch (SESSION_TYPE) {
   return await createSessionStateLess(userId)
   // 🐶 Lève une erreur en cas de type invalide
   // 🤖 throw new Error('Invalid session type')
 }
 
-// 🐶 Implemente le Strategy Pattern
+// 🐶 Implémente le `Strategy Pattern`
 export async function verifySession() {
   return await verifySessionStateless()
 }
 
-// 🐶 Implemente le Strategy Pattern
+// 🐶 Implémente le `Strategy Pattern`
 export async function updateSession() {
   return await updateSessionStateLess()
 }
 
-// 🐶 Implemente le Strategy Pattern
+// 🐶 Implémente le `Strategy Pattern`
 export function deleteSession() {
   return deleteSessionStateLess()
 }
