c57c3236852a409a0f171ab7b4c9d565347387c6
Mike Codeur <contact@mikecodeur.com>
2024-11-01 16:52:03 +0800

    update next 15

--- src/app/instructions/layout.tsx
+++ src/app/instructions/layout.tsx
@@ -1,1 +1,2 @@
-export {default} from '../exercises/layout'
+export {/* @next-codemod-error `default` export is re-exported. Check if this component uses `params` or `searchParams`*/
+default} from '../exercises/layout'
